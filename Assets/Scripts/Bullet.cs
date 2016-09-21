f71b6c7867a3d36012b1025c2b78b8c79b2414ba
Seanmcfarlane97 <Seanmcfarlane97@gmail.com>
2016-09-21 14:24:23 +1000

    Initial commit.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+public class Bullet : MonoBehaviour {
+
+    public Combat combat; 
+
+	void OnCollisionEnter(Collision col)
+	{
+		combat = col.gameObject.GetComponent<Combat>(); //Get the script "Combat"
+		if (combat != null) //If the object as the Combat script attatched 
+		{
+			combat.TakeDamage(10); //Deduct a set amount from the objects health
+		}
+
+		Destroy(gameObject); //Destroy the bullet
+	}
+
+}
---
+++ Assets/Scripts/PlayerNet.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerNet : NetworkBehaviour
+{
+	public Camera childCam;
+	public GameObject bullet;
+	public GameObject barrel;
+	public override void OnStartLocalPlayer()
+	{
+        //Enable an example player controller
+		GetComponent<FPSInputController>().enabled = true; 
+		GetComponent<CharacterMotor>().enabled = true;
+        GetComponent<MouseLook>().enabled = true; 
+
+        //Set the camera to active
+        childCam.gameObject.SetActive(true);
+		childCam.enabled = true;
+
+	}
+
+	void Update()
+	{
+		if (!isLocalPlayer)
+			return;
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			CmdShoot();
+		}
+	}
+
+	[Command]
+	void CmdShoot()
+	{
+		// create bullets on each client, not spawned on server
+		RpcShoot();
+	}
+
+	[ClientRpc]
+	void RpcShoot()
+	{
+		GameObject b = Instantiate(bullet, barrel.transform.position, transform.rotation) as GameObject; //Spawn the prefab as a gameobject
+		b.GetComponent<Rigidbody>().velocity = transform.forward * 20; //Send the gameobject forward
+		Destroy(b, 3.0f); //Destroy the gameobject after a time
+	}
+}
